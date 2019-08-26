a6aa5994788513df03b77a7b5c5ff5e0a4cbdfe9
hanati <hanati@tezign.com>
2019-08-26 14:39:29 +0800

    test

--- crnn_model/vgg.py
+++ crnn_model/vgg.py
@@ -162,7 +162,7 @@ def vgg_16(inputs,
         end_points_collection = sc.original_name_scope + '_end_points'
         # Collect outputs for conv2d, fully_connected and max_pool2d.
         with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
-                            outputs_collections=end_points_collection,):
+                            outputs_collections=end_points_collection, reuse=tf.AUTO_REUSE):
             net = slim.repeat(inputs, 2, slim.conv2d, 64, [3, 3], scope='conv1')
             net = slim.max_pool2d(net, [2, 2], scope='pool1')
             net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
