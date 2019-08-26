f052eb917928a84299c5d79b260c8b8c8e2885f7
hanati <hanati@tezign.com>
2019-08-26 17:12:33 +0800

    add reuse for multi_gpu train

--- config/global_config.py
+++ config/global_config.py
@@ -51,7 +51,7 @@ __C.TRAIN.GPU_MEMORY_FRACTION = 0.9
 # Set the GPU allow growth parameter during tensorflow training process
 __C.TRAIN.TF_ALLOW_GROWTH = True
 # Set the shadownet training batch size
-__C.TRAIN.BATCH_SIZE = 64
+__C.TRAIN.BATCH_SIZE = 128
 # Set the shadownet validation batch size
 __C.TRAIN.VAL_BATCH_SIZE = 32
 # Set the learning rate decay steps
