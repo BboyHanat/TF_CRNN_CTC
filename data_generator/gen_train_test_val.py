a33eb45ff942e8e76643e9821bce30582c223139
hanati <hanati@tezign.com>
2019-08-27 23:09:55 +0800

    modified to multi process excute

--- data_generator/gen_train_test_val.py
+++ data_generator/gen_train_test_val.py
@@ -9,7 +9,9 @@ Desc:
 import os
 import random
 
-data_fp = open("/hanat/data/data.txt",'r')
+num_thread = 16
+data_file = [open("/hanat/data/data_"+str(index)+'.txt','r') for index in range(num_thread)]
+
 train_fp = open("/hanat/data/train.txt", 'w')
 val_fp = open("/hanat/data/val.txt", 'w')
 test_fp = open("/hanat/data/test.txt", 'w')
@@ -17,20 +19,25 @@ test_fp = open("/hanat/data/test.txt", 'w')
 val_num_interval = 9
 test_num_interval = 10
 count = 0
-while True:
-    line = data_fp.readline()
-    if not line:
-        break
-    i = random.randint(0, 10)
-    if count % val_num_interval == 0 and count >= val_num_interval:
-        val_fp.write(line)
-    elif count % test_num_interval == 0 and count >= test_num_interval:
-        test_fp.write(line)
-    else:
-        train_fp.write(line)
-    count += 1
+for i in range(num_thread):
+    data_fp = data_file[i]
+    while True:
+        line = data_fp.readline()
+        if not line:
+            break
+        i = random.randint(0, 10)
+        if count % val_num_interval == 0 and count >= val_num_interval:
+            val_fp.write(line)
+        elif count % test_num_interval == 0 and count >= test_num_interval:
+            test_fp.write(line)
+        else:
+            train_fp.write(line)
+        count += 1
 
-data_fp.close()
+for i in range(num_thread):
+    data_file[i].close()
+
+print("File num is {}".format(str(count)))
 train_fp.close()
 val_fp.close()
 test_fp.close()
