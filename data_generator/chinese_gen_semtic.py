83f1e332b4fba898b9f57fab60d6425c94684deb
hanati <hanati@tezign.com>
2019-09-03 10:53:04 +0800

    semtic dataset generate

--- data_generator/chinese_gen_semtic.py
+++ data_generator/chinese_gen_semtic.py
@@ -335,26 +335,26 @@ def random_paste(image, font_bg, x1, y1, x2, y2):
 
 ######################################################
 
-# path_chinese_synthetic = "./sentence.txt"
-# path_img = '/hanat/ocr_background_img'
-# path_font = '/data/User/hanat/TF_CRNN_CTC/data/fonts'
-# path_have_yen_path = '/data/User/hanat/TF_CRNN_CTC/data/have_yen_fonts'     #'/data/User/hanat/TF_CRNN_CTC/data/have_yen_fonts'
-# path_save = '/hanat/data3/image_data'
-# annotation_file = '/hanat/data3/data_'
-# font_size_range = (30, 100)
-# process_num = 16
-# batch_r = 14458
-
-
 path_chinese_synthetic = "./sentence.txt"
-path_img = '/Users/aidaihanati/TezignProject/PSD/替换素材(1)/images/background'
-path_font = '/Users/aidaihanati/TezignProject/TF_CRNN_CTC/data/fonts'
-path_have_yen_path = '/Users/aidaihanati/TezignProject/TF_CRNN_CTC/data/fonts'
-path_save = './hanat/train'
-annotation_file = './hanat/data_'
+path_img = '/hanat/ocr_background_img'
+path_font = '/data/User/hanat/TF_CRNN_CTC/data/fonts'
+path_have_yen_path = '/data/User/hanat/TF_CRNN_CTC/data/have_yen_fonts'     #'/data/User/hanat/TF_CRNN_CTC/data/have_yen_fonts'
+path_save = '/hanat/data3/image_data'
+annotation_file = '/hanat/data3/data_'
 font_size_range = (30, 100)
-process_num = 4
-batch_r = 10
+process_num = 16
+batch_r = 14458
+
+
+# path_chinese_synthetic = "./sentence.txt"
+# path_img = '/Users/aidaihanati/TezignProject/PSD/替换素材(1)/images/background'
+# path_font = '/Users/aidaihanati/TezignProject/TF_CRNN_CTC/data/fonts'
+# path_have_yen_path = '/Users/aidaihanati/TezignProject/TF_CRNN_CTC/data/fonts'
+# path_save = './hanat/train'
+# annotation_file = './hanat/data_'
+# font_size_range = (30, 100)
+# process_num = 4
+# batch_r = 10
 
 os.makedirs(path_save, exist_ok=True)
 fp = open(path_chinese_synthetic, "r")
